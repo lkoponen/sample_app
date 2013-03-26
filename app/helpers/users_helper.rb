380460c930f45dfe15f341ee98f267f127b100e5
Lili Koponen <ljkoponen@gmail.com>
2013-03-25 17:30:07 -0700

    Add Exercises from Chapter 7

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,10 @@
 module UsersHelper
 
 	# Returns the Gravatar for the given user.
-	def gravatar_for(user)
+	def gravatar_for(user, options = { size: 50 })
 	gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-	gravatr_url = "http://www.gravatar.com/avatar/#{gravatar_id}"
+	size = options[:size]
+	gravatr_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
 	image_tag(gravatr_url, alt: user.name, class: "gravatar")
 	end
 
